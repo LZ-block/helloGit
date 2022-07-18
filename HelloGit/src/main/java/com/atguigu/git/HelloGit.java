89d4a11084d1c4968085c78b427ba5b17dad2d77
liyuandong <1825377224@qq.com>
2022-07-18 17:10:32 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author PeterLi
+ * @date 2022/7/18 - 16:09
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
